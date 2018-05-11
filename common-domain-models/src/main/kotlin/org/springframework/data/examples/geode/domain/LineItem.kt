cd5028401d559152383ca030f59ef189bfa90470
Udo Kohlmeyer <ukohlmeyer@pivotal.io>
2018-05-11 16:03:24 -0700

    Converted common domain models into Kotlin Added basicOperation for client-server

---
+++ common-domain-models/src/main/kotlin/org/springframework/data/examples/geode/domain/LineItem.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.examples.geode.domain
+
+import org.springframework.util.Assert
+import java.io.Serializable
+import java.math.BigDecimal
+
+/**
+ * @author Oliver Gierke
+ */
+class LineItem
+/**
+ * Constructs a new [LineItem] for the given [Product] and amount.
+ *
+ * @param product must not be null.
+ * @param amount the number of [Products][Product] purchased.
+ */
+@JvmOverloads constructor(private val product: Product, private val amount: Int = 1) : Serializable {
+    val unitPrice: BigDecimal
+    val productId = product.id
+    val productName = product.name
+
+    val total: BigDecimal
+        get() = unitPrice.multiply(BigDecimal.valueOf(amount.toLong()))
+
+    init {
+        Assert.isTrue(amount > 0, "The amount of Products to be bought must be greater than 0")
+        this.unitPrice = product.price
+    }
+
+    override fun toString(): String = "Purchased $amount of Product $productName at $unitPrice for total of $total"
+
+    companion object {
+        private const val serialVersionUID = 6571108615511329726L
+    }
+}
---
+++ common-domain-models/src/main/kotlin/org/springframework/data/examples/geode/domain/Order.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.examples.geode.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.gemfire.mapping.annotation.Region
+import java.io.Serializable
+import java.math.BigDecimal
+import java.util.*
+
+/**
+ * @author Oliver Gierke
+ * @author David Turanski
+ * @author Udo Kohlmeyer
+ */
+@Region
+data class Order @JvmOverloads constructor(@Id @javax.persistence.Id val id: Long?, val customerId: Long, val billingAddress: Address, val shippingAddress: Address = billingAddress) : Serializable, Iterable<LineItem> {
+    private val lineItems = HashSet<LineItem>()
+
+    /**
+     * Returns the total of the [Order].
+     *
+     * @return
+     */
+    val total: BigDecimal
+        get() {
+
+            var total = BigDecimal.ZERO
+
+            for (item in this) {
+                total = total.add(item.total)
+            }
+
+            return total
+        }
+
+    /**
+     * Adds the given [LineItem] to the [Order].
+     *
+     * @param lineItem
+     */
+    fun add(lineItem: LineItem) = lineItems.add(lineItem)
+
+    /**
+     * Returns all [LineItem]s currently belonging to the [Order].
+     *
+     * @return
+     */
+    fun getLineItems(): Set<LineItem> = Collections.unmodifiableSet(lineItems)
+
+    override fun iterator(): Iterator<LineItem> = getLineItems().iterator()
+
+    override fun toString(): String = id?.toString() ?: "0"
+
+    companion object {
+        private val serialVersionUID = -3779061453639083037L
+    }
+}
