47e609dff78d93c201fcec069d23014e257f07f1
Patrick Johnson <pjohnson@pivotal.io>
2019-07-31 12:10:52 -0700

    Add a println to runners in Java to be more consistent with Kotlin version

--- server-config-examples/region-config/src/main/java/example/springdata/geode/server/region/config/RegionTypeConfiguration.java
+++ server-config-examples/region-config/src/main/java/example/springdata/geode/server/region/config/RegionTypeConfiguration.java
@@ -80,7 +80,10 @@ public class RegionTypeConfiguration {
     class EmbeddedLocator {
         @Bean
         public ApplicationRunner runner() {
-            return args -> new Scanner(System.in).nextLine();
+            return args -> {
+                System.err.println("Press <ENTER> to exit");
+                new Scanner(System.in).nextLine();
+            };
         }
     }
 
@@ -90,7 +93,10 @@ public class RegionTypeConfiguration {
     class PeerServer {
         @Bean
         public ApplicationRunner runner() {
-            return args -> new Scanner(System.in).nextLine();
+            return args -> {
+                System.err.println("Press <ENTER> to exit");
+                new Scanner(System.in).nextLine();
+            };
         }
     }
 
@@ -108,6 +114,8 @@ public class RegionTypeConfiguration {
 
                 createOrders(productRepository, orderRepository);
 
+                System.err.println("Press <ENTER> to exit");
+
                 new Scanner(System.in).nextLine();
 
             };
@@ -117,15 +125,15 @@ public class RegionTypeConfiguration {
             Random random = new Random(System.nanoTime());
             Address address = new Address("it", "doesn't", "matter");
             LongStream.rangeClosed(1, 100).forEach((orderId) ->
-                    LongStream.rangeClosed(1, 3000).forEach((customerId) -> {
-                        Order order = new Order(orderId, customerId, address);
-                        IntStream.rangeClosed(0, random.nextInt(3) + 1).forEach((lineItemCount) -> {
-                            int quantity = random.nextInt(3) + 1;
-                            long productId = random.nextInt(3) + 1;
-                            order.add(new LineItem(productRepository.findById(productId).get(), quantity));
-                        });
-                        orderRepository.save(order);
-                    }));
+                LongStream.rangeClosed(1, 3000).forEach((customerId) -> {
+                    Order order = new Order(orderId, customerId, address);
+                    IntStream.rangeClosed(0, random.nextInt(3) + 1).forEach((lineItemCount) -> {
+                        int quantity = random.nextInt(3) + 1;
+                        long productId = random.nextInt(3) + 1;
+                        order.add(new LineItem(productRepository.findById(productId).get(), quantity));
+                    });
+                    orderRepository.save(order);
+                }));
         }
 
         private void createProducts(ProductRepository productRepository) {
@@ -133,9 +141,7 @@ public class RegionTypeConfiguration {
                     "An Apple portable music player"));
             productRepository.save(new Product(2L, "Apple iPad", new BigDecimal("499.99"),
                     "An Apple tablet device"));
-            Product
-                    macbook =
-                    new Product(3L, "Apple macBook", new BigDecimal("899.99"),
+            Product macbook = new Product(3L, "Apple macBook", new BigDecimal("899.99"),
                             "An Apple notebook computer");
             macbook.addAttribute("warranty", "included");
             productRepository.save(macbook);
@@ -144,10 +150,10 @@ public class RegionTypeConfiguration {
         private void createCustomerData(CustomerRepository customerRepository) {
             System.out.println("Inserting 3 entries for keys: 1, 2, 3");
             LongStream.rangeClosed(0, 3000)
-                    .parallel()
-                    .forEach(customerId ->
-                            customerRepository.save(new Customer(customerId, new EmailAddress(customerId + "@2.com"), "John" + customerId, "Smith" + customerId)));
+                .parallel()
+                .forEach(customerId ->
+                        customerRepository.save(new Customer(customerId, new EmailAddress(customerId + "@2.com"),
+                                "John" + customerId, "Smith" + customerId)));
         }
     }
-
 }
