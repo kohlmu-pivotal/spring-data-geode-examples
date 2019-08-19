64a9e407b3cbf8092a2a1dad7cf7e4315b5c05ca
Patrick Johnson <pjohnson@pvotal.io>
2019-08-19 13:46:40 -0700

    Add test for OQL querry example

--- client-server-examples/oql-queries/src/test/java/examples.springdata.geode.client.oql/OQLClientTest.java
+++ client-server-examples/oql-queries/src/test/java/examples.springdata.geode.client.oql/OQLClientTest.java
@@ -1,11 +1,61 @@
 package examples.springdata.geode.client.oql;
 
+import examples.springdata.geode.client.common.server.Server;
+import examples.springdata.geode.client.oql.services.CustomerService;
+import examples.springdata.geode.domain.Customer;
+import examples.springdata.geode.domain.EmailAddress;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.data.gemfire.tests.integration.ForkingClientServerIntegrationTestsSupport.startGemFireServer;
+
+@ActiveProfiles({"test", "default"})
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class OQLClientTest {
 
+    @Autowired
+    private CustomerService customerService;
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        startGemFireServer(Server.class);
+    }
+
+    @Test
+    public void customerServiceWasConfiguredCorrectly() {
+
+        assertThat(this.customerService).isNotNull();
+    }
+
+    @Test
+    public void customerRepositoryWasAutoConfiguredCorrectly() {
+
+        Customer jonDoe = new Customer(15L, new EmailAddress("example@example.org"), "Jon", "Doe");
+
+        this.customerService.save(jonDoe);
+
+        Customer jon2 = (Customer)this.customerService.findByEmailAddress("example@example.org").get(0);
+        assertThat(jon2).isEqualTo(jonDoe);
+
+        jon2 = (Customer) this.customerService.findByFirstNameUsingIndex("Jon").get(0);
+        assertThat(jon2).isEqualTo(jonDoe);
+
+        jon2 = this.customerService.findById(15L).get();
+        assertThat(jon2).isEqualTo(jonDoe);
+
+        String query = "select * from /Customers customer where customer.lastName=$1";
+        jon2 = (Customer) this.customerService.findWithTemplate(query, "Doe").get(0);
+        assertThat(jon2).isEqualTo(jonDoe);
+
+        assertThat(this.customerService.findByFirstNameLocalClientRegion(query, "Doe")).isEmpty();
+    }
 }
