0a9ec1ba0686dee2d5df018dd0404494a2569e11
Patrick Johnson <pjohnson@pvotal.io>
2019-09-05 11:21:15 -0700

    Move data population code out of config an into server

--- server-config-examples/region-config/src/main/java/examples/springdata/geode/server/region/config/RegionTypeConfiguration.java
+++ server-config-examples/region-config/src/main/java/examples/springdata/geode/server/region/config/RegionTypeConfiguration.java
@@ -1,14 +1,13 @@
 package examples.springdata.geode.server.region.config;
 
-import examples.springdata.geode.domain.*;
+import examples.springdata.geode.domain.Customer;
+import examples.springdata.geode.domain.Order;
+import examples.springdata.geode.domain.Product;
 import examples.springdata.geode.server.region.repo.CustomerRepository;
-import examples.springdata.geode.server.region.repo.OrderRepository;
-import examples.springdata.geode.server.region.repo.ProductRepository;
 import org.apache.geode.cache.DataPolicy;
 import org.apache.geode.cache.GemFireCache;
 import org.apache.geode.cache.PartitionAttributes;
 import org.apache.geode.cache.RegionAttributes;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.gemfire.PartitionAttributesFactoryBean;
@@ -18,12 +17,8 @@ import org.springframework.data.gemfire.ReplicatedRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.PeerCacheApplication;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
-import java.math.BigDecimal;
-import java.util.Random;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-
 @Configuration
+@PeerCacheApplication
 @EnableGemfireRepositories(basePackageClasses = CustomerRepository.class)
 public class RegionTypeConfiguration {
 
@@ -68,60 +63,5 @@ public class RegionTypeConfiguration {
         partitionedRegionFactoryBean.setDataPolicy(DataPolicy.PARTITION);
         partitionedRegionFactoryBean.setAttributes(regionAttributes);
         return partitionedRegionFactoryBean;
-    }
-
-    @Configuration
-    @PeerCacheApplication(name = "dataPopulationPeer")
-    class DataPopulationPeerServerKT {
-        @Bean
-        public ApplicationRunner runner(CustomerRepository customerRepository, OrderRepository orderRepository,
-                                        ProductRepository productRepository) {
-            return args -> {
-                createCustomerData(customerRepository);
-
-                createProducts(productRepository);
-
-                createOrders(productRepository, orderRepository);
-
-                System.out.println("There are " + customerRepository.count() + " customers");
-                System.out.println("There are " + productRepository.count() + " products");
-                System.out.println("There are " + orderRepository.count() + " orders");
-            };
-        }
-
-        private void createOrders(ProductRepository productRepository, OrderRepository orderRepository) {
-            Random random = new Random(System.nanoTime());
-            Address address = new Address("it", "doesn't", "matter");
-            LongStream.rangeClosed(1, 100).forEach((orderId) ->
-                LongStream.rangeClosed(1, 3000).forEach((customerId) -> {
-                    Order order = new Order(orderId, customerId, address);
-                    IntStream.rangeClosed(0, random.nextInt(3) + 1).forEach((lineItemCount) -> {
-                        int quantity = random.nextInt(3) + 1;
-                        long productId = random.nextInt(3) + 1;
-                        order.add(new LineItem(productRepository.findById(productId).get(), quantity));
-                    });
-                    orderRepository.save(order);
-                }));
-        }
-
-        private void createProducts(ProductRepository productRepository) {
-            productRepository.save(new Product(1L, "Apple iPod", new BigDecimal("99.99"),
-                    "An Apple portable music player"));
-            productRepository.save(new Product(2L, "Apple iPad", new BigDecimal("499.99"),
-                    "An Apple tablet device"));
-            Product macbook = new Product(3L, "Apple macBook", new BigDecimal("899.99"),
-                            "An Apple notebook computer");
-            macbook.addAttribute("warranty", "included");
-            productRepository.save(macbook);
-        }
-
-        private void createCustomerData(CustomerRepository customerRepository) {
-            System.out.println("Inserting 3 entries for keys: 1, 2, 3");
-            LongStream.rangeClosed(0, 3000)
-                .parallel()
-                .forEach(customerId ->
-                        customerRepository.save(new Customer(customerId, new EmailAddress(customerId + "@2.com"),
-                                "John" + customerId, "Smith" + customerId)));
-        }
     }
 }
--- server-config-examples/region-config/src/main/kotlin/examples/springdata/geode/server/region/kt/config/RegionTypeConfigurationKT.kt
+++ server-config-examples/region-config/src/main/kotlin/examples/springdata/geode/server/region/kt/config/RegionTypeConfigurationKT.kt
@@ -1,14 +1,13 @@
 package examples.springdata.geode.server.region.kt.config
 
-import examples.springdata.geode.domain.*
+import examples.springdata.geode.domain.Customer
+import examples.springdata.geode.domain.Order
+import examples.springdata.geode.domain.Product
 import examples.springdata.geode.server.region.kt.repo.CustomerRepositoryKT
-import examples.springdata.geode.server.region.kt.repo.OrderRepositoryKT
-import examples.springdata.geode.server.region.kt.repo.ProductRepositoryKT
 import org.apache.geode.cache.DataPolicy
 import org.apache.geode.cache.GemFireCache
 import org.apache.geode.cache.PartitionAttributes
 import org.apache.geode.cache.RegionAttributes
-import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -18,12 +17,9 @@ import org.springframework.data.gemfire.RegionAttributesFactoryBean
 import org.springframework.data.gemfire.ReplicatedRegionFactoryBean
 import org.springframework.data.gemfire.config.annotation.PeerCacheApplication
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories
-import java.math.BigDecimal
-import java.util.*
-import java.util.stream.IntStream
-import java.util.stream.LongStream
 
 @Configuration
+@PeerCacheApplication
 @EnableGemfireRepositories(basePackageClasses = [CustomerRepositoryKT::class])
 class RegionTypeConfigurationKT {
 
@@ -69,62 +65,5 @@ class RegionTypeConfigurationKT {
         partitionedRegionFactoryBean.dataPolicy = DataPolicy.PARTITION
         partitionedRegionFactoryBean.attributes = regionAttributes
         return partitionedRegionFactoryBean
-    }
-
-    @Configuration
-    @PeerCacheApplication(name = "dataPopulationPeer")
-    internal inner class DataPopulationPeerServerKT {
-        @Bean
-        fun runner(customerRepository: CustomerRepositoryKT, orderRepository: OrderRepositoryKT,
-                   productRepository: ProductRepositoryKT): ApplicationRunner {
-            return ApplicationRunner {
-                createCustomerData(customerRepository)
-
-                createProducts(productRepository)
-
-                createOrders(productRepository, orderRepository)
-
-                println("There are " + customerRepository.count() + " customers")
-                println("There are " + productRepository.count() + " products")
-                println("There are " + orderRepository.count() + " orders")
-            }
-        }
-
-        private fun createOrders(productRepository: ProductRepositoryKT, orderRepository: OrderRepositoryKT) {
-            val random = Random(System.nanoTime())
-            val address = Address("it", "doesn't", "matter")
-            LongStream.rangeClosed(1, 100).forEach { orderId ->
-                LongStream.rangeClosed(1, 3000).forEach { customerId ->
-                    val order = Order(orderId, customerId, address)
-                    IntStream.rangeClosed(0, random.nextInt(3) + 1).forEach { lineItemCount ->
-                        val quantity = random.nextInt(3) + 1
-                        val productId = (random.nextInt(3) + 1).toLong()
-                        order.add(LineItem(productRepository.findById(productId).get(), quantity))
-                    }
-                    orderRepository.save(order)
-                }
-            }
-        }
-
-        private fun createProducts(productRepository: ProductRepositoryKT) {
-            productRepository.save(Product(1L, "Apple iPod", BigDecimal("99.99"),
-                    "An Apple portable music player"))
-            productRepository.save(Product(2L, "Apple iPad", BigDecimal("499.99"),
-                    "An Apple tablet device"))
-            val macbook = Product(3L, "Apple macBook", BigDecimal("899.99"),
-                    "An Apple notebook computer")
-            macbook.addAttribute("warranty", "included")
-            productRepository.save(macbook)
-        }
-
-        private fun createCustomerData(customerRepository: CustomerRepositoryKT) {
-            println("Inserting 3 entries for keys: 1, 2, 3")
-            LongStream.rangeClosed(0, 3000)
-                    .parallel()
-                    .forEach { customerId ->
-                        customerRepository.save(Customer(customerId, EmailAddress("$customerId@2.com"),
-                                "John$customerId", "Smith$customerId"))
-                    }
-        }
     }
 }
